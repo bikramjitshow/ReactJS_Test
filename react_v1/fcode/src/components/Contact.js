0d1c62d186fd3aa6b4b70a68cba3231df3e5b226
Bikramjit <bikramjitshow2016@gmail.com>
2021-06-07 14:56:48 +0530

    form

--- react_v1/fcode/src/components/Contact.js
+++ react_v1/fcode/src/components/Contact.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
     <div>
       <h1>Contact</h1>
+
+      <form className="contact-form">
+
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Contact;

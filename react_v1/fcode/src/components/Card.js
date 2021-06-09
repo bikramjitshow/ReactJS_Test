022d4d0aa6b1c6f16b6588e7df9d5b4da965ed56
Bikramjit <bikramjitshow2016@gmail.com>
2021-06-09 09:27:45 +0530

    code check

--- react_v1/fcode/src/components/Card.js
+++ react_v1/fcode/src/components/Card.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Card({cdata}) {
   return (
     <div className="card">
       <h1>{cdata.title}</h1>
       <p>{cdata.desc}</p>
+      <Link to="#" class="btn btn-primary stretched-link">View More</Link>
     </div>
 
   );
--- react_v1/fcode/src/components/Courses.js
+++ react_v1/fcode/src/components/Courses.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import Card from './Card';
 
 function Courses() {
+
+  const [cdatas, setcdatas] = useState([
+    { title: 'Computer Science: Programming with a Purpose', desc: 'Description of card A' },
+    { title: 'Card B', desc: 'Description of card B' },
+    { title: 'Card C', desc: 'Description of card C' },
+    { title: 'Card D', desc: 'Description of card D' },
+    { title: 'Card E', desc: 'Description of card E' },
+    { title: 'Card F', desc: 'Description of card F' },
+    { title: 'Card G', desc: 'Description of card G' },
+    { title: 'Card H', desc: 'Description of card H' },
+  ]);
+
   return (
     <div>
       <h1>Courses</h1>
+      <div className="card-wrap row">
+
+
+        {
+          cdatas.length > 0 ?
+            cdatas.map((item) =>
+              <div className="col-3">
+                <Card cdata={item}></Card>
+              </div>
+            ) : <p className='no-data-txt'>!! No Cards Found !!</p>
+        }
+
+        {/* !! No Cards Found !! */}
+
+      </div>
     </div>
   )
 }
